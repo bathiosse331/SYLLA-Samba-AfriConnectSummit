caadb6c8bb9c48956fc38b53cb96b86da72d07af
Samba Sylla <bathsylla03@gmail.com>
2026-07-08 11:32:02 +0000

    Navbar resonsive sur telephone et filtrage dynamique des intervenants

--- js/main.js
+++ js/main.js
@@ -12,35 +12,60 @@ programme.forEach((pro)=>{
 // compteur qui s'incremente au scroll
 let participant = 0;
 let nbr_arrive = 12000;
-let interval = setInterval(()=>{
+let parrticipant = document.getElementById("participant");
+if(parrticipant){
+    let interval = setInterval(()=>{
+    parrticipant.innerHTML = participant;
     participant++;
-    document.getElementById("participant").innerHTML = participant;
-    if(participant >= nbr_arrive){clearInterval(interval);}
-},30)
+    if(participant >= nbr_arrive){
+        clearInterval(interval);
+    }
+},30);
+}
+    
 
-let intervenant = 0;
+
+let intervenan = 0;
 let intervenant_arrive = 48;
-let intervalle = setInterval(()=>{
-    intervenant++;
-    document.getElementById("intervenant").innerHTML = intervenant;
-    if(intervenant >= intervenant_arrive){clearInterval(intervalle);}
+let intervenant = document.getElementById("intervenant");
+if(intervenant){
+    let intervalle = setInterval(()=>{
+    intervenant.innerHTML = intervenan;
+    intervenan++;
+    if(intervenan >= intervenant_arrive){
+        clearInterval(intervalle);
+    }
 },100)
+    
+}
 
 let jour = 0;
 let jour_arrive = 3;
-let intervall = setInterval(()=>{
+let jours = document.getElementById("jour");
+if(jours){
+    let intervall = setInterval(()=>{
+    jour.innerHTML = jour;
     jour++;
-    document.getElementById("jour").innerHTML = jour;
-    if(jour >= jour_arrive){clearInterval(intervall);}
-},1000)
+    if(jour >= jour_arrive){
+        clearInterval(intervall);
+    }
 
-let pays = 0;
+},1000)
+}
+
+let pay = 0;
 let pays_arrive = 12;
-let interva = setInterval(()=>{
-    pays++;
-    document.getElementById("pays").innerHTML = pays;
-    if(pays >= pays_arrive){clearInterval(interva);}
+let pays = document.getElementById("pays");
+if(pays){
+    let interva = setInterval(()=>{
+    pays.innerHTML = pay;
+    pay++;
+    if(pay >= pays_arrive){
+        clearInterval(interva);
+    }
+
 },200)
+}
 
 
 
@@ -51,4 +76,26 @@ const navMenu = document.querySelector('.nav_menu');
 burgerBtn.addEventListener('click', () => {
     // Ajoute .open si elle n'y est pas, la retire si elle y est
     navMenu.classList.toggle('open');
+});
+
+
+
+// filtrage des intervenants
+
+let btne = document.querySelectorAll(".filtre")
+let carte = document.querySelectorAll(".profil")
+btne.forEach((bt) =>{
+  bt.addEventListener("click",function(){
+    let filter = this.getAttribute("data-cat")
+    carte.forEach((cart) =>{
+      let cat = cart.getAttribute("data-target")
+      if (filter === "tous"){
+        cart.style.display = "";
+      }else if(filter === cat){
+        cart.style.display = "";
+      }else{
+        cart.style.display = "none";
+      }
+    });
+  });
 });
